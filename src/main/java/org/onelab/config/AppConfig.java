fff4e8918d3433e36ca663db876ca3a4df547566
Alibek Abilkassymov <al16ek199@gmail.com>
2025-02-28 17:22:53 +0500

    added datasource bean in appconfig.class

--- src/main/java/org/onelab/config/AppConfig.java
+++ src/main/java/org/onelab/config/AppConfig.java
@@ -4,11 +4,19 @@ import org.onelab.repoimpl.OrderRepoImpl;
 import org.onelab.repoimpl.ProductRepoImpl;
 import org.onelab.repoimpl.UserRepoImpl;
 import org.onelab.service.AppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class AppConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(AppConfig.class);
 
     @Bean
     public OrderRepoImpl orderRepo() {
@@ -28,5 +36,18 @@ public class AppConfig {
     @Bean
     public AppService service() {
         return new AppService(orderRepo(), productRepo(), userRepo());
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        try {
+            return new EmbeddedDatabaseBuilder()
+                    .setType(EmbeddedDatabaseType.H2)
+                    .addScript("classpath:jdbc/schema.sql")
+                    .addScript("classpath:jdbc/test-data.sql").build();
+        } catch (Exception e) {
+            logger.error("Embedded datasource been cannot be created!", e);
+            return null;
+        }
     }
 }
