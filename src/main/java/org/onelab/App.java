a7714fc3a6de617ab7767b38d5f1fe94fe2b77c6
Alibek Abilkassymov <al16ek199@gmail.com>
2025-03-04 20:44:29 +0500

    added springboot dependency, removed unnecessary dependencies

--- src/main/java/org/onelab/App.java
+++ src/main/java/org/onelab/App.java
@@ -1,42 +1,12 @@
 package org.onelab;
 
-import org.onelab.config.AppConfig;
-import org.onelab.model.OrderProducts;
-import org.onelab.model.Orders;
-import org.onelab.model.Product;
-import org.onelab.model.Users;
-import org.onelab.repoimpl.OrderRepoImpl;
-import org.onelab.repoimpl.UserRepoImpl;
-import org.onelab.service.AppService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
+@SpringBootApplication
 
 public class App {
     public static void main(String[] args) {
-
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        Users user1 = Users.builder()
-                .id(4)
-                .firstname("name4")
-                .lastname("surname4")
-                .build();
-
-        Orders order1 = Orders.builder()
-                .id(4)
-                .userId(4)
-                .build();
-
-        AppService appService = context.getBean(AppService.class);
-
-        appService.saveUser(user1);
-        appService.findAllUsers();
-        appService.findAllOrders();
-        appService.findUserById(1);
-        appService.saveOrder(order1);
-        appService.findUserById(10);
+        SpringApplication.run(App.class, args);
     }
 }
