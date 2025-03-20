0bc4f4485a68947401450af2d79b2c26b86fcf76
Alibek Abilkassymov <al16ek199@gmail.com>
2025-03-20 19:05:55 +0500

    added gateway

---
+++ gateway/src/main/java/com/onelab/gateway/config/ApiGatewayConfig.java
@@ -0,0 +1,43 @@
+package com.onelab.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/products/**")
+                        .filters(f -> f
+                                .prefixPath("/api")
+                                .addResponseHeader("X-Powered-By", "Gateway Service")
+                        )
+                        .uri("http://localhost:8081")
+                )
+                .route(r -> r.path("/inventory/**")
+                        .filters(f -> f
+                                .prefixPath("/api")
+                                .addResponseHeader("X-Powered-By", "Gateway Service")
+                        )
+                        .uri("http://localhost:8082")
+                )
+                .route(r -> r.path("/orders/**")
+                        .filters(f -> f
+                                .prefixPath("/api")
+                                .addResponseHeader("X-Powered-By", "Gateway Service")
+                        )
+                        .uri("http://localhost:8083")
+                )
+                .route(r -> r.path("/auth/**")
+                        .filters(f -> f
+                                .addResponseHeader("X-Powered-By", "Gateway Service")
+                        )
+                        .uri("http://localhost:8084")
+                )
+                .build();
+    }
+}
